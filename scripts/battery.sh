03b4dd9a24f1d6e3a3f7f012bbb84d152e387210
Matti Juntti <matti.juntti@pm.me>
2019-08-24 10:26:06 +0300

    Fix trailing comma issue in battery charge vals < 10

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 stat=$( acpi | awk '{print $3}' | cut -c -8 )
-batt=$( acpi | awk '{print $4}' | cut -c -3 )
+batt=$( acpi | awk '{print $4}' | tr -dc '0-9' )
 
 if [ $stat == "Charging" ]; then
-        echo "⌁ $batt"
+        echo "⌁ $batt%"
 
 elif [ $stat == "Full," ]; then
-        echo "✔ $batt"
+        echo "✔ $batt%"
 
 else
-        echo "♥ $batt"
+        echo "♥ $batt%"
 fi
