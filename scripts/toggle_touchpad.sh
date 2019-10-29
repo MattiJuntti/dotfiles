fa05739e127a408f2cf4c6707dfb28b2615ece15
Matti Juntti <matti.juntti@pm.me>
2019-10-29 10:13:40 +0200

    Add toggle_touchpad.sh

---
+++ scripts/toggle_touchpad.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                                                      
+
+DEVICE='Elan Touchpad'                                              
+
+ENABLED=$(xinput list-props "$DEVICE" | awk '/^\tDevice Enabled \([0-9]+\):\t[01]/ {print $NF}')
+case $ENABLED in                                                                
+  0)                                                                            
+    xinput enable "$DEVICE"                                                     
+    echo "$DEVICE enabled"                                                      
+    ;;                                                                          
+  1)                                                                            
+    xinput disable "$DEVICE"                                                     
+    echo "$DEVICE disabled"                                                      
+    ;;                                                                          
+  *)                                                                            
+    echo                                                                        
+    xinput list --name-only                                                     
+    ;;                                                                          
+esac
